9bee37f62a33acc1cd27675830d5d5b134ebc08a
kochakman999-source <kochakman999@gmail.com>
2026-08-18 15:28:27 +0800

    Configure Firebase web app

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
-// Replace these values with your Firebase Web App configuration.
 export const firebaseConfig = {
-  apiKey: "你的Firebase apiKey",
+  apiKey: "AIzaSyBT3BmDTF625_J6J9vmwMMHMUnK1iSSOmI",
   authDomain: "etf-core-portfolio.firebaseapp.com",
   projectId: "etf-core-portfolio",
   storageBucket: "etf-core-portfolio.firebasestorage.app",
-  messagingSenderId: "你的messagingSenderId",
-  appId: "你的appId"
+  messagingSenderId: "480034410970",
+  appId: "1:480034410970:web:1eb0ab7b86768dce73b0e3",
+  measurementId: "G-R5FVYKTV8Y"
 };
