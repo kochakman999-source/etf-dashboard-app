52dfc36a4bce6528f56803d8201f62c16be5adab
kochakman999-source <kochakman999@gmail.com>
2026-09-07 09:30:38 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'etf-portfolio-safari-login-v4';
+const CACHE_NAME = 'etf-portfolio-safari-login-v5';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
